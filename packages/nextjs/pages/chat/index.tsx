7578f29aaed1a46c00314bcb81371994db2921c0
oemerfurkan <omerfurkanyuruk1@gmail.com>
2023-11-19 04:19:17 +0300

    chat

--- packages/nextjs/pages/chat/index.tsx
+++ packages/nextjs/pages/chat/index.tsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
 export default function Home() {
-    return <></>
+  const [message, setMessage] = useState("");
+
+  return (
+    <>
+      <div className="w-full flex flex-col h-[760px] justify-end gap-3">
+        <div className="px-48">
+            Deneme
+        </div>
+        <div className="w-full flex focus:outline-none gap-6 justify-center items-center">
+          <input
+            className="flex gap-5 rounded-xl w-3/4 h-16 bg-white input-box px-4 text-[20px]"
+            onChange={(e) => setMessage(e.currentTarget.value)}
+          ></input>
+          <button className="w-[150px] h-[70px] bg-[#3DB9CF] rounded-xl">
+            Send
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
