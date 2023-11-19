80d1e9b67470f331541523349cf9a8f6dc4985de
uok825 <omerleroyunda@gmail.com>
2023-11-19 08:25:47 +0300

    wagmichains

--- packages/nextjs/context/Web3Modal.tsx
+++ packages/nextjs/context/Web3Modal.tsx
@@ -3,20 +3,22 @@
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
 
 import { WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, hardhat } from 'viem/chains'
+import { 
+  scrollSepolia, sepolia, hardhat, baseGoerli, mantleTestnet
+ } from 'viem/chains'
 
 // 1. Get projectId
 const projectId = '87b19285bd7e81f2a2dc76728f6eb331'
 
 // 2. Create wagmiConfig
 const metadata = {
-  name: 'Web3Modal',
-  description: 'Web3Modal Example',
+  name: 'meETH',
+  description: 'Domain Service',
   url: 'https://web3modal.com',
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-const chains = [mainnet, arbitrum, hardhat]
+const chains = [scrollSepolia, sepolia, mantleTestnet, baseGoerli, hardhat]
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
 
 // 3. Create modal
